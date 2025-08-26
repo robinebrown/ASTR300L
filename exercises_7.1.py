e723d2b3094fffb3f507af91c9504004be41ab0b
Robin Brown <robinebrown2@gmail.com>
2025-08-26 13:08:46 -1000

    initial commit

---
+++ exercises_7.1.py
@@ -0,0 +1,11 @@
+'''
+1. Write a Python script which computes the gravitational force between (a) the
+Earth and the Moon, and (b) The Sun and the Moon (you will need to look
+up masses and mean distances). The code should print the result as a number
+formatted to two decimal places. This last step involves looking up how to
+output formatted numbers in Python.
+'''
+
+mass_earth = 5.972 * (10**24)
+mass_moon = 7.34767309 * (10**22)
+print(mass_moon)
